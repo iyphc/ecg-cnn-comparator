82aadee9d232c391e3efa906089a048803cbe7b1
iyphc <werhistudy@yandex.ru>
2025-07-10 12:24:07 +0300

    Additional functions

---
+++ src/models/utils.py
@@ -0,0 +1,20 @@
+import torch
+from src.data.preprocess import load_ECG_dataset
+from torch.utils.data import DataLoader, random_split
+
+def get_device():
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        return torch.device("mps")
+    else:
+        return torch.device("cpu")
+    
+def get_dataloaders(valid_part=0.2, batch_size=128, num_workers=2):
+    train_set, test_set, classes = load_ECG_dataset()
+    
+    d_train, d_val = random_split(train_set, [1 - valid_part, valid_part])
+    train_loader = DataLoader(d_train, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    val_loader = DataLoader(d_val, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=num_workers)    
+    return train_loader, test_loader, val_loader, classes

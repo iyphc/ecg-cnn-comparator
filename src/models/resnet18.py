2f92724431f0e8e7a6d2a864d9b9021da84a741b
iyphc <werhistudy@yandex.ru>
2025-07-11 13:45:33 +0300

    initialized vars

--- src/models/resnet18.py
+++ src/models/resnet18.py
@@ -2,9 +2,20 @@ import torch
 import torch.nn as nn
 import torch.functional as f
 
+def downsample(in_channels, out_channels, stride):
+        seq = nn.Sequential(
+            nn.Conv1d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+            nn.BatchNorm1d(out_channels)
+        )
+        return seq
+
 class BasicModule(nn.Module):
     def __init__(self, in_channels: int, out_channels: int, stride=1, kernel_size=12):
         super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.stride = stride
+        self.kernel_size = kernel_size
         self.seq = nn.Sequential(
             nn.Conv1d(in_channels, out_channels, kernel_size, stride=stride, padding=1, bias=False),
             nn.BatchNorm1d(out_channels),
@@ -12,18 +23,13 @@ class BasicModule(nn.Module):
             nn.Conv1d(out_channels, out_channels, kernel_size, stride=stride, padding=1, bias=False),
             nn.BatchNorm1d(out_channels),
         )
-        
-    def downsample(self, x):
-        seq = nn.Sequential(
-            nn.Conv1d(self.in_channels, self.out_channels, kernel_size=1, stride=self.stride, bias=False),
-            nn.BatchNorm1d(self.out_channels)
-        )
-        return seq
+
     def forward(self, x):
         input = x.copy()
         x = self.seq(x)
         if self.in_channels != self.out_channels:
-            input = downsample(input)
+            seq = downsample(self.in_channels, self.out_channels, self.stride)
+            input = seq(input)
         return x + input
 
 class ResNet(nn.Module):
@@ -38,8 +44,8 @@ class ResNet(nn.Module):
 
             # Система блоков
             
-            '''
-            IN PROGRESS
-            '''
+            # -----------
+            # IN PROGRESS
+            # -----------
         )
         

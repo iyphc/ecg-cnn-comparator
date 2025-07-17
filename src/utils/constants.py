53ca7f48f457b661aa521634cf47090fc55c482c
iyphc <werhistudy@yandex.ru>
2025-07-17 16:43:14 +0300

    #6: refactored

--- src/utils/constants.py
+++
@@ -1,7 +0,0 @@
-REDUCED_DISEASES_LIST = [
-    "SR",     # Sinus rhythm
-    "SARRH",   # Arrhythmia
-    "SBRAD",  # Sinus bradycardia
-    "STACH",  # Sinus tachycardia
-    "AFIB",   # Atrial fibrillation
-]

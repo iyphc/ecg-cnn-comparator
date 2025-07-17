6763cd4bf1e8c6faa143bdaefe9cbb99e836fd7d
iyphc <werhistudy@yandex.ru>
2025-07-17 17:54:20 +0300

    #5: init initialization + refactoring

---
+++ src/ecg_analyzer/training/__init__.py
@@ -0,0 +1,9 @@
+from .trainer import train_model, validate
+from .evaluator import evaluate_model, basic_scores
+
+__all__ = [
+    "train_model",
+    "validate", 
+    "evaluate_model",
+    "basic_scores",
+]

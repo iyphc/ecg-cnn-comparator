0a27fe0fc3119f8c2da4bdc4d0777fd54c0b1d93
iyphc <werhistudy@yandex.ru>
2025-07-21 15:23:14 +0300

    #5: created economical data loading to dataset

--- src/ecg_analyzer/data/preprocess.py
+++ src/ecg_analyzer/data/preprocess.py
@@ -6,52 +6,8 @@ import torch
 import numpy as np
 import pandas as pd
 from torch.utils.data import Dataset
-from sklearn.preprocessing import MultiLabelBinarizer
-
-
-class ECG_Dataset(Dataset):
-    def __init__(
-        self,
-        signals=None,
-        handcrafted_features=None,
-        labels=None,
-        use_signals=True,
-        use_handcrafted=True,
-    ):
-        self.use_signals = use_signals
-        self.use_handcrafted = use_handcrafted
-
-        if signals is not None:
-            self.signals = torch.tensor(signals, dtype=torch.float32)
-        else:
-            self.signals = None
-
-        if handcrafted_features is not None:
-            self.handcrafted = torch.tensor(handcrafted_features, dtype=torch.float32)
-        else:
-            self.handcrafted = None
-
-        if labels is not None:
-            self.labels = torch.tensor(labels, dtype=torch.float32)
-        else:
-            raise ValueError("Labels must be provided")
-
-    def __len__(self):
-        return len(self.labels)
-
-    def __getitem__(self, idx):
-        x_signal = (
-            self.signals[idx].transpose(0, 1)
-            if self.use_signals and self.signals is not None
-            else None
-        )
-        x_handcrafted = (
-            self.handcrafted[idx]
-            if self.use_handcrafted and self.handcrafted is not None
-            else None
-        )
-        y = self.labels[idx]
-        return x_signal, x_handcrafted, y
+from sklearn.preprocessing import MultiLabelBinarizer, StandardScaler
+from .ecg_dataset import ECG_Dataset
 
 
 def visualise_ecg_by_id(df, ecg_id, sampling_rate, path):
@@ -73,22 +29,15 @@ def visualise_ecg_by_id(df, ecg_id, sampling_rate, path):
     )
 
 
-def load_raw_data(df, sampling_rate, path):
+def load_data_names(df, sampling_rate, path):
     filenames = df.filename_lr if sampling_rate == 100 else df.filename_hr
+
     data = []
 
-    for i, filename in enumerate(filenames):
-        try:
-            signal, _ = wfdb.rdsamp(path + filename)
-            data.append(signal)
-        except Exception as e:
-            data.append(None)
-            print(f"Failed to load file {filename}: {e}")
+    for filename in filenames:
+        data.append(path + filename)
 
-    if not data:
-        raise ValueError("No data was successfully loaded")
-
-    return np.array(data, dtype=np.float32)
+    return data
 
 
 def normalize(signals: np.ndarray) -> np.ndarray:
@@ -101,38 +50,48 @@ def normalize(signals: np.ndarray) -> np.ndarray:
     return X
 
 
-def handcrafted_extraction(df: pd.DataFrame, features):
-    df = df.copy()
+def normalize_only_non_binary(feature_values):
+    if feature_values.size == 0:
+        return feature_values
+
     binary_features = []
     non_binary_features = []
+
+    for i in range(feature_values.shape[1]):
+        col = feature_values[:, i]
+        unique_values = np.unique(col[~np.isnan(col)])
+        if len(unique_values) <= 2 and all(val in [0, 1, -1] for val in unique_values):
+            binary_features.append(col)
+        else:
+            non_binary_features.append(col)
+
+    if binary_features:
+        binary_features = np.column_stack(binary_features)
+    else:
+        binary_features = np.empty((feature_values.shape[0], 0))
+
+    if non_binary_features:
+        non_binary_features = np.column_stack(non_binary_features)
+        non_binary_features = normalize(non_binary_features)
+    else:
+        non_binary_features = np.empty((feature_values.shape[0], 0))
+
+    features = np.hstack([binary_features, non_binary_features])
+    return features
+
+
+def handcrafted_extraction(df: pd.DataFrame, features):
+    df = df.copy()
+
+    feature_values = []
 
     for feature in features:
         if feature not in df.columns:
             print(f"Признак '{feature}' не найден в датафрейме")
             continue
+        feature_values.append(df[feature].values)
 
-        feature_values = df[feature].values
-        unique_values = np.unique(feature_values[~np.isnan(feature_values)])
-
-        if len(unique_values) <= 2 and all(val in [0, 1, -1] for val in unique_values):
-            binary_features.append(feature_values)
-            print(f"Признак '{feature}' определен как бинарный: {unique_values}")
-        else:
-            non_binary_features.append(feature_values)
-            print(
-                f"Признак '{feature}' определен как небинарный: диапазон [{np.min(unique_values):.2f}, {np.max(unique_values):.2f}]"
-            )
-
-    binary_features = (
-        np.column_stack(binary_features) if binary_features else np.empty((len(df), 0))
-    )
-    non_binary_features = (
-        np.column_stack(non_binary_features)
-        if non_binary_features
-        else np.empty((len(df), 0))
-    )
-
-    return binary_features, non_binary_features
+    return feature_values
 
 
 def process_dataset(
@@ -151,17 +110,21 @@ def process_dataset(
             f"SCP statements file not found at {path}scp_statements.csv"
         )
 
+    features = features if features else []
+
     Y = pd.read_csv(path + "ptbxl_database.csv", index_col="ecg_id")
     Y["scp_codes"] = Y["scp_codes"].apply(ast.literal_eval)
 
-    X = load_raw_data(Y, sampling_rate, path)
-    X_binary, X_non_binary = handcrafted_extraction(Y, features)
+    X = load_data_names(Y, sampling_rate, path)
+    X = np.array(X)
 
-    X_non_binary = normalize(X_non_binary)
-    X = normalize(X)
-    X_handcrafted = np.hstack([X_binary, X_non_binary])
+    X_handcrafted = handcrafted_extraction(Y, features)
+    X_handcrafted = np.array(X_handcrafted)
 
-    features_list = features if features else []
+    if X_handcrafted.size > 0:
+        X_handcrafted = X_handcrafted.T
+    else:
+        X_handcrafted = np.zeros((len(X), 0))
 
     idx = [i for i, x in enumerate(X) if x is not None]
     X = X[idx]
@@ -205,24 +168,26 @@ def process_dataset(
     X_val = X[mask_val.to_numpy()]
     X_test = X[mask_test.to_numpy()]
 
+    handcrafted_train = X_handcrafted[mask_train.to_numpy()]
+    handcrafted_val = X_handcrafted[mask_val.to_numpy()]
+    handcrafted_test = X_handcrafted[mask_test.to_numpy()]
+
+    handcrafted_train = normalize_only_non_binary(handcrafted_train)
+
     y_train = np.array(Y[mask_train]["one_hot"].tolist(), dtype=np.float32)
     y_val = np.array(Y[mask_val]["one_hot"].tolist(), dtype=np.float32)
     y_test = np.array(Y[mask_test]["one_hot"].tolist(), dtype=np.float32)
 
     os.makedirs("data/processed", exist_ok=True)
 
-    handcrafted_train = X_handcrafted[mask_train.to_numpy()]
-    handcrafted_val = X_handcrafted[mask_val.to_numpy()]
-    handcrafted_test = X_handcrafted[mask_test.to_numpy()]
-
     train_dataset = ECG_Dataset(
-        signals=X_train, labels=y_train, handcrafted_features=handcrafted_train
+        signals_names=X_train, labels=y_train, handcrafted_features=handcrafted_train
     )
     val_dataset = ECG_Dataset(
-        signals=X_val, labels=y_val, handcrafted_features=handcrafted_val
+        signals_names=X_val, labels=y_val, handcrafted_features=handcrafted_val
     )
     test_dataset = ECG_Dataset(
-        signals=X_test, labels=y_test, handcrafted_features=handcrafted_test
+        signals_names=X_test, labels=y_test, handcrafted_features=handcrafted_test
     )
 
     # Всегда сохраняем файлы (перезаписываем существующие)
@@ -232,7 +197,7 @@ def process_dataset(
     torch.save(mlb.classes_, "data/processed/diseases_names.pt")
 
     print("Data saved successfully!")
-    return train_dataset, val_dataset, test_dataset, mlb.classes_, features_list
+    return train_dataset, val_dataset, test_dataset, mlb.classes_, features
 
 
 def load_ECG_dataset(
@@ -280,3 +245,7 @@ def load_ECG_dataset(
 
     print("Data loaded")
     return train_dataset, val_dataset, test_dataset, diseases_names, features_list
+
+
+if __name__ == "__main__":
+    process_dataset()
